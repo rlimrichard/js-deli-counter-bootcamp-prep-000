5046abb56ac41bcc7022bc90027fa327c9b2defb
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-21 20:13:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(array, name){
     var position = array.length ;
     return `Welcome, ${name}. You are number ${position} in line.`
 }
+
+function nowServing(array){
+    
+    if (array.length > 0){
+        var name = array.shift()
+        return `Currently serving ${name}`
+    }
+    return "There is nobody waiting to be served!"
+}
